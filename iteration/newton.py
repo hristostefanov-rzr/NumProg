503cffdc33a62e7f10b719e26d0db6fca22c3517
hristostefanov-rzr <hsmtga@gmail.com>
2023-09-15 17:24:25 +0200

    Finished adding the algorithms

--- iteration/newton.py
+++ iteration/newton.py
@@ -3,16 +3,31 @@ from misc import Polynomial
 
 # Newton's method for finding roots of a polynomial
 # Takes as an input a polynomial and returns a root
-def newton_method(p, x0, k=1, eps=1e-5, max_iter=100):
-    p_prim = get_derivative(p.coeff)
+def newton_method_polynomial(p, x0, k=1, eps=1e-5, max_iter=100):
+    p_prim = get_derivative_polynomial(p.coeff)
+    return newton_method(p, p_prim, x0, k, eps, max_iter)
+
+
+# Newton's method for finding roots of a function when the derivative is known
+def newton_method(f, f_prim, x0, k=1, eps=1e-5, max_iter=100):
     x = x0
     for i in range(max_iter):
-        x = x - k * (p(x) / p_prim(x))
-        if abs(p(x)) < eps:
+        x = x - k * (f(x) / f_prim(x))
+        if abs(f(x)) < eps:
             return x
     return x
 
 
 # Takes as an input a polynomial and returns its derivative polynomial
-def get_derivative(p):
+def get_derivative_polynomial(p):
     return Polynomial([p[i] * i for i in range(1, len(p))])
+
+# Secant method for finding roots of a function without knowing the derivative
+def secant_method(f, x0, x1, eps=1e-5, max_iter=100):
+    x = x1
+    x_prev = x0
+    for i in range(max_iter):
+        x, x_prev = x - (f(x) * (x - x_prev)) / (f(x) - f(x_prev)), x
+        if abs(f(x)) < eps:
+            return x
+    return x
--- ode/approximate.py
+++ ode/approximate.py
@@ -1,3 +1,7 @@
+from iteration.newton import secant_method
+
+
+# Implementations of approximate methods for solving ODEs
 def eulers_method(t0, y0, f, delta_t, target_t):
     y = y0
     t = t0
@@ -31,4 +35,22 @@ def runge_kutta_method(t0, y0, f, delta_t, target_t):
         T4 = f(t + delta_t, y + delta_t * T3)
 
         y = y + (delta_t / 6) * (T1 + 2 * T2 + 2 * T3 + T4)
+    return y
+
+
+# A somewhat buggy implementation of the implicit Euler's method
+
+
+# Implicit Euler that uses secant method to solve the equation
+def implicit_euler_method(t0, y0, f, delta_t, target_t):
+    steps = int((target_t - t0) / delta_t)
+    y = y0
+    t = t0
+    for i in range(steps):
+        t = t + delta_t
+
+        def function_to_solve(next_y):
+            return y + delta_t * f(t, next_y) - next_y
+
+        y = secant_method(function_to_solve, y - 1, y + 1)
     return y
